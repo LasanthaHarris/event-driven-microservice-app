f381a6478e0c9f65d0e83630c588bf328d597ad8
LasanthaHarris <lasanthaharris@gmail.com>
2021-11-10 23:23:59 +0530

    initial commit of eureka discovery service

---
+++ discovery-service/src/test/java/home/and/company/eventdriven/shoppingcart/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package home.and.company.eventdriven.shoppingcart.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
